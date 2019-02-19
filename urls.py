04042ea37d054313f1325bc1c4798d02374bae6d
philippbruhin <info@philippbruhin.ch>
2019-02-19 22:21:11 +0100

    added router

--- urls.py
+++ urls.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url, include
+from django.contrib import admin
+from django.urls import path, include
 from aldryn_django.utils import i18n_patterns
 import aldryn_addons.urls
 
 
 urlpatterns = [
     # add your own patterns here
+    path('', include('leads.urls')),
 ] + aldryn_addons.urls.patterns() + i18n_patterns(
     # add your own i18n patterns here
     *aldryn_addons.urls.i18n_patterns()  # MUST be the last entry!
