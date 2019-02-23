4a0533e4a62d17199993b4db6241bf623b9b8389
philippbruhin <info@philippbruhin.ch>
2019-02-23 16:31:18 +0100

    removed authentication in api

--- leads/urls.py
+++ leads/urls.py
@@ -1,3 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from aldryn_django.utils import i18n_patterns
+import aldryn_addons.urls
+
 from rest_framework import routers
 from .api import LeadViewSet
 
